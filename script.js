78fdb9a78089426be53602397029a60438f72c4f
karawaqqe <146225027+karawaqqe@users.noreply.github.com>
2026-01-05 02:02:42 +0100

    update

--- script.js
+++ script.js
@@ -120,8 +120,7 @@ var init = function () {
       q: ~~(rand() * heartPointsCount),
       D: 2 * (i % 2) - 1,
       force: 0.2 * rand() + 0.7,
-      f: "rgb(255, 20, 147)"
-,
+      f: "rgb(255, 20, 147)",
       trace: Array.from({ length: traceCount }, () => ({ x, y })),
     };
   }
